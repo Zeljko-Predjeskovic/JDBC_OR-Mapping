82e3779f394f75b7a7b7f61a3d511dffeb558c8d
Zeljko Predjeskovic <pre18309@spengergasse.at>
2021-02-16 23:08:33 +0100

    DO db connection with tables test

--- src/test/java/spengergasse/presistable/LemonadeDataAccessObjectTest.java
+++ src/test/java/spengergasse/presistable/LemonadeDataAccessObjectTest.java
@@ -1,15 +1,32 @@
 package spengergasse.presistable;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import spengergasse.model.Lemonade;
 import spengergasse.persistence.LemonadeDataAccessObject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 public class LemonadeDataAccessObjectTest {
 
     private LemonadeDataAccessObject lemonadeDataAccessObject;
+
+
+    @BeforeEach
+    void initializeDB(){
+        try{
+            Connection connection = DriverManager.getConnection("jdbc:h2:mem:test");
+            connection.createStatement().execute("");
+            lemonadeDataAccessObject = new LemonadeDataAccessObject(connection);
+
+        }catch(SQLException e){
+            throw new RuntimeException("Failed to connect to database", e);
+        }
+    }
 
     @Test
     void assertFindAll(){
